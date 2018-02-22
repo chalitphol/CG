5757d999e098b364d45193b6a23ad522d1443e8c
chalitphol kuensuwan <chalitphol@gmail.com>
2018-02-23 00:22:14 +0700

    third commit

--- main.cpp
+++ main.cpp
@@ -2,14 +2,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h> 
-static GLfloat dx1 = 425.0, dy1 = 0.0;
-static GLfloat dx2 = 425.0, dy2 = 0.0;
-static GLfloat dx3 = 425.0, dy3 = 0.0;
+static GLfloat dx1 = 425.0, dy1 = -50.0, ds1 = 2;
+static GLfloat dx2 = 425.0, dy2 = -85.0, ds2 = 4;
+static GLfloat dx3 = 425.0, dy3 = -135.0, ds3 = 6;
 static float flowerM = true;
 static float flowerMS = 0;
 
-#define PI 3.14159265
-#define Convert PI/180
+static float road[2][3] = { {{80.0 / 255} ,{ 80.0 / 255 } ,{ 80.0 / 255 }} ,{{ 160.0 / 255 },{ 160.0 / 255 },{ 160.0 / 255 }}};
+static float ground[2][3] = { { { 150.0 / 255 } ,{ 90.0 / 255 } ,{ 20.0 / 255 } } ,{ { 130.0 / 255 },{ 70.0 / 255 },{ 0.0 / 255 } } };
+static float sky[2][3] = { { { 30.0 / 255 } ,{ 206.0 / 255 } ,{ 240.0 / 255 } } ,{ { 30.0 / 255 },{ 150.0 / 255 },{ 250.0 / 255 } } };
+
+static int light = 0;
+static int smoke = 0;
+
+static float period = 0;
+static int timeCount = 0;
+
+static float firstCar[3] = { 220.0,220.0, 220.0 };
+static float secondCar[3] = { 255.0,220.0,0 };
+static float thirdCar[3] = { 0,180.0,255 };
+
+
 
 
 void init(void)
@@ -58,7 +71,7 @@ void flower2(float r1, float r2, float x, float y, float height, float num, floa
 	circle(r1 / 2.25, x + flowerMS, y, 360);
 	glLineWidth(4.0);
 	glBegin(GL_LINES);
-	glVertex2f(x + flowerMS, y - r1);
+	glVertex2f(x + flowerMS, y - r1 + 2);
 	glVertex2f(x , y - r1 - height);
 	glEnd();
 
@@ -68,9 +81,9 @@ void garden(float r1, float r2, float x1, float x2, float y, float height, float
 
 	srand(time(NULL));
 	for (int i = x1; i <= x2; i += 35) {
-		float c1 = (rand() % 255 + 1);
-		float c2 = (rand() % 230 + 1);
-		float c3 = (rand() % 230 + 1);
+		float c1 = (rand() % 255 + 50);
+		float c2 = (rand() % 255 + 50);
+		float c3 = (rand() % 255 + 50);
 		height = rand() % 25 + 2 * r1;
 		glColor3f(c1 / 255, c2 / 255, c3 / 255);
 		flower2(r1, r2, i , y + height, height, num, rotate + 90);
@@ -143,6 +156,22 @@ void car(float x, float y, float offset, float c1, float c2, float c3) {
 	glVertex2f(x + 82.5 * offset, y - 35 * offset);
 	glEnd();
 
+	if (smoke <= 7 && smoke > 0) {
+		glColor4f(150.0 / 255, 150.0 / 255, 150.0 / 255 ,0.5);
+		circle(10, x + 84.5 * offset, y - 32.5 * offset , 360);
+	}
+	else if (smoke <= 14) {
+		glColor4f(150.0 / 255, 150.0 / 255, 150.0 / 255, 0.5);
+		circle(10, x + 84.5 * offset, y - 32.5 * offset, 360);
+		circle(15, x + 99.5 * offset, y - 32.5 * offset, 360);
+	}
+	else if (smoke <= 21) {
+		glColor4f(150.0 / 255, 150.0 / 255, 150.0 / 255, 0.5);
+		circle(10, x + 84.5 * offset, y - 32.5 * offset, 360);
+		circle(15, x + 99.5 * offset, y - 32.5 * offset, 360);
+		circle(20, x + 124.5 * offset, y - 32.5 * offset, 360);
+	}
+
 	glColor3f(190.0 / 255, 190.0 / 255, 190.0 / 255);//กันชนหลัง
 	glBegin(GL_POLYGON);
 	glVertex2f(x + 70 * offset, y - 35 * offset);
@@ -179,15 +208,39 @@ void car(float x, float y, float offset, float c1, float c2, float c3) {
 	glVertex2f(x - 1.5 * offset, y - 15.0 * offset);
 	glEnd();
 
-	glColor3f(240.0 / 255, 240.0 / 255, 240.0 / 255);//ไฟหน้า
-	glBegin(GL_POLYGON);
-	glVertex2f(x - 75 * offset, y - 22.5 * offset);
-	glVertex2f(x - 75 * offset, y - 10 * offset);
-	glColor3f(180.0 / 255, 180.0 / 255, 180.0 / 255);
-	glVertex2f(x - 55 * offset, y - 10 * offset);
-	glVertex2f(x - 55 * offset, y - 17.5 * offset);
-	glVertex2f(x - 62.5 * offset, y - 22.5 * offset);
-	glEnd();
+	if (light == 0) {
+		glColor3f(240.0 / 255, 240.0 / 255, 240.0 / 255);//ไฟหน้า
+		glBegin(GL_POLYGON);
+		glVertex2f(x - 75 * offset, y - 22.5 * offset);
+		glVertex2f(x - 75 * offset, y - 10 * offset);
+		glColor3f(180.0 / 255, 180.0 / 255, 180.0 / 255);
+		glVertex2f(x - 55 * offset, y - 10 * offset);
+		glVertex2f(x - 55 * offset, y - 17.5 * offset);
+		glVertex2f(x - 62.5 * offset, y - 22.5 * offset);
+		glEnd();
+	}
+	else {
+		glColor3f(240.0 / 255, 240.0 / 255, 0.0 / 255);//ไฟหน้า
+		glBegin(GL_POLYGON);
+		glVertex2f(x - 75 * offset, y - 22.5 * offset);
+		glVertex2f(x - 75 * offset, y - 10 * offset);
+		glColor3f(180.0 / 255, 180.0 / 255, 180.0 / 255);
+		glVertex2f(x - 55 * offset, y - 10 * offset);
+		glVertex2f(x - 55 * offset, y - 17.5 * offset);
+		glVertex2f(x - 62.5 * offset, y - 22.5 * offset);
+		glEnd();
+
+		glColor4f(240.0 / 255, 240.0 / 255, 0.0 / 255,0.8);
+		glBegin(GL_POLYGON);
+		glVertex2f(x - 75 * offset, y - 22.5 * offset);
+		glVertex2f(x - 75 * offset, y - 10 * offset);
+		glColor4f(255.0 / 255, 255.0 / 255, 0.0 / 255,0.5);
+		glVertex2f(x - 175 * offset, y - 45 * offset);
+		glVertex2f(x - 125 * offset, y - 45 * offset);
+		glEnd();
+
+		ellipse(50 * offset, 12.5 * offset, x - 135 * offset, y - 45 * offset, 0);
+	}
 
 	glColor3f(180.0 / 255, 180.0 / 255, 180.0 / 255);//ไฟหลัง
 	glBegin(GL_POLYGON);
@@ -283,12 +336,12 @@ void car(float x, float y, float offset, float c1, float c2, float c3) {
 	glColor3f(180.0 / 255, 180.0 / 255, 180.0 / 255);
 	glVertex2f(x, y + 3 * offset);
 	glVertex2f(x, y + 30 * offset);
-	glVertex2f(x * offset, y + 30 * offset);
+	glVertex2f(x, y + 30 * offset);
 	glEnd();
 
 	glColor3f(150.0 / 255, 150.0 / 255, 150.0 / 255); //ตัดเส้นกระจกข้าง
 	glBegin(GL_LINES);
-	glVertex2f(x - 17.5* offset, y + 30 * offset);
+	glVertex2f(x - 17.5 * offset, y + 30 * offset);
 	glVertex2f(x - 32.5 * offset, y + 10.5 * offset);
 	glEnd();
 	glBegin(GL_LINES);
@@ -305,10 +358,10 @@ void car(float x, float y, float offset, float c1, float c2, float c3) {
 	glEnd();
 	glBegin(GL_LINES);
 	glVertex2f(x, y + 30 * offset);
-	glVertex2f(x * offset, y + 30 * offset);
+	glVertex2f(x, y + 30 * offset);
 	glEnd();
 	glBegin(GL_LINES);
-	glVertex2f(x * offset, y + 30 * offset);
+	glVertex2f(x , y + 30 * offset);
 	glVertex2f(x - 17.5* offset, y + 30 * offset);
 	glEnd();
 
@@ -426,32 +479,54 @@ void house(float x, float y, float offset) {
 }
 
 void draw() {
-	glColor3f(80.0 / 255, 80.0 / 255, 80.0 / 255);//ถนน
+	glColor3f(road[0][0] - 20.0 * period / 255, road[0][1] - 20.0 * period / 255, road[0][2] - 20.0 * period / 255);//ถนน
 	glBegin(GL_POLYGON);
 	glVertex2f(330, -170);
 	glVertex2f(-330, -170);
-	glColor3f(160.0 / 255, 160.0 / 255, 160.0 / 255);
+	glColor3f(road[1][0] - 20.0 * period / 255, road[1][1] - 20.0 * period / 255, road[1][2] - 20.0 * period / 255);
 	glVertex2f(-330, -80);
 	glVertex2f(330, -80);
 	glEnd();
 
-	glColor3f(150.0 / 255, 90.0 / 255, 20 / 255);//ดิน
+	glColor3f(ground[0][0] - 20.0 * period / 255, ground[0][1] - 20.0 * period / 255, ground[0][2] - 20.0 * period / 255);//ดิน
 	glBegin(GL_POLYGON);
 	glVertex2f(330, -80);
 	glVertex2f(-330, -80);
-	glColor3f(130.0 / 255, 70.0 / 255, 0 / 255);
+	glColor3f(ground[1][0] - 20.0 * period / 255, ground[1][1] - 20.0 * period / 255, ground[1][2] - 20.0 * period / 255);
 	glVertex2f(-330, -20);
 	glVertex2f(330, -20);
 	glEnd();
 
-	glColor3f(30.0 / 255, 206.0 / 255, 240.0 / 255);//ท้องฟ้า
-	glBegin(GL_POLYGON);
-	glVertex2f(330, -20);
-	glVertex2f(-330, -20);
-	glColor3f(30.0 / 255, 150.0 / 255, 250.0 / 255);
-	glVertex2f(-330, 170);
-	glVertex2f(330, 170);
-	glEnd();
+	if (period == 0) {
+		glColor3f(sky[0][0], sky[0][1], sky[0][2]);//ท้องฟ้า
+		glBegin(GL_POLYGON);
+		glVertex2f(330, -20);
+		glVertex2f(-330, -20);
+		glColor3f(sky[1][0], sky[1][1], sky[1][2]);
+		glVertex2f(-330, 170);
+		glVertex2f(330, 170);
+		glEnd();
+	}
+	else if (period == 1) {
+		glColor3f(245.0 / 255, 190.0 / 255, 0);
+		glBegin(GL_POLYGON);
+		glVertex2f(330, -20);
+		glVertex2f(-330, -20);
+		glColor3f(195.0 / 255, 105.0 / 255, 0);
+		glVertex2f(-330, 170);
+		glVertex2f(330, 170);
+		glEnd();
+	}
+	else {
+		glColor3f(25.0 / 255, 25.0 / 255, 112.0 / 255);//ท้องฟ้า
+		glBegin(GL_POLYGON);
+		glVertex2f(330, -20);
+		glVertex2f(-330, -20);
+		glColor3f(0.0 / 255, 0.0/255, 20.0/255);
+		glVertex2f(-330, 170);
+		glVertex2f(330, 170);
+		glEnd();
+	}
 
 	house(150, -75, 2);
 
@@ -461,24 +536,73 @@ void draw() {
 	fense(-330, 40, -75, 2);
 	fense(255, 330, -75, 2);
 
-	car(dx1, -65, 1, 220.0, 220.0, 220.0);
+	car(dx1, dy1,1 + (-dy1 - 50)*0.005, firstCar[0], firstCar[1], firstCar[2]);
+	car(dx2, dy2, 1 + (-dy2 - 50)*0.005, secondCar[0], secondCar[1], secondCar[2]);
+	car(dx3, dy3, 1 + (-dy3 - 50)*0.005, thirdCar[0], thirdCar[1], thirdCar[2]);
+
 
 	if (dx1 <= -425) {
 		dx1 = 425;
+		dy1 = -(rand() % 25 + 50);
+		ds1 = 2 + (rand() % 2 + 1);
+		firstCar[0] = rand() % 255 + 50;
+		firstCar[1] = rand() % 255 + 50;
+		firstCar[2] = rand() % 255 + 50;
 	}
 	else
-		dx1 -= 5;
+		dx1 -= ds1;
+
+	if (dx2 <= -425) {
+		dx2 = 425;
+		dy2 = -(rand() % 25 + 85);
+		ds2 = 4 + (rand() % 2 + 1);
+		secondCar[0] = rand() % 255 + 50;
+		secondCar[1] = rand() % 255 + 50;
+		secondCar[2] = rand() % 255 + 50;
+	}
+	else
+		dx2 -= ds2;
+
+	if (dx3 <= -425) {
+		dx3 = 425;
+		dy3 = -(rand() % 25 + 135);
+		ds3 = 6 + (rand() % 2 + 1);
+		thirdCar[0] = rand() % 255 + 50;
+		thirdCar[1] = rand() % 255 + 50;
+		thirdCar[2] = rand() % 255 + 50;
+	}
+	else
+		dx3 -= ds3;
+
+
 	if (flowerM == true) {
-		flowerMS--;
+		flowerMS-= 0.5;
 		if (flowerMS< -10)
 			flowerM = false;
 	}
 	else {
-		flowerMS++;
+		flowerMS+=0.5;
 		if (flowerMS >= 10)
 			flowerM = true;
 	}
 
+	if (smoke >= 21) {
+		smoke = 0;
+	}
+	else {
+		smoke++;
+	}
+
+	timeCount++;
+	if (timeCount == 180) {
+		timeCount = 0;
+		period++;
+		light++;
+		if (period > 2) {
+			period = 0;
+			light = 0;
+		}
+	}
 }
 
 
@@ -509,7 +633,7 @@ void mouse(int button, int state, int x, int y)
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1366, 768);
 	glutInitWindowPosition(300, 300);
 	glutCreateWindow("CG-HW1");
